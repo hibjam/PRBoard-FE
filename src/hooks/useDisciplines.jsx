f6854c5dd30747ca40e5c22fea68d4e7401d1598
James Hibbert <jhibbert@spartaglobal.com>
2026-03-25 17:17:49 +0000

    Use proper hook

--- src/hooks/useDisciplines.jsx
+++ src/hooks/useDisciplines.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-const API_BASE = ''
+const API_BASE = import.meta.env.VITE_API_BASE ?? ''
 
 export function useDisciplines(getHeaders) {
   const [disciplines, setDisciplines] = useState([])
